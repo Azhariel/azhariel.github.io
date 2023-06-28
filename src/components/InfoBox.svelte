cafa5a769328adc24f6c6d98485a976ebe77f155
Renan Braga <azhariel@gmail.com>
2023-06-28 10:26:15 -0300

    feat: add information box

---
+++ src/components/InfoBox.svelte
@@ -0,0 +1,8 @@
+<div class="flex flex-1 border-primary-400 border mt-8 h-32 w-2/4 mx-auto">
+	<div class="flex border border-red-700 mx-auto place-items-center">
+		<p>information</p>
+	</div>
+	<div class="flex border border-red-700 mx-auto place-items-center">
+		<p>description</p>
+	</div>
+</div>
