dbbd353e96617951c4eb525abdc3bc37e10912bf
Renan Braga <azhariel@gmail.com>
2023-06-28 10:26:02 -0300

    feat: add components

---
+++ src/components/Hero.svelte
@@ -0,0 +1,19 @@
+<div class="w-2/3 h-1/2 mx-auto mt-4">
+	<div class="bg-hero bg-cover bg-center bg-no-repeat w-full h-full" />
+	<div class="container relative bottom-24 mx-auto flex-1 justify-center text-center w-100">
+		<h2 class="text-4xl font-bold tracking-widest text-primary-400">Web Developer</h2>
+		<h1 class="text-8xl text-secondary-500 text-center">Renan Braga</h1>
+	</div>
+</div>
+
+<style>
+	.bg-hero {
+		background-image: url('/170423_zha_16.png');
+		mask-image: linear-gradient(
+			to bottom,
+			rgba(0, 0, 0, 1) 75%,
+			rgba(0, 0, 0, 0.6) 90%,
+			rgba(0, 0, 0, 0)
+		);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,7 @@
-<div class="w-2/3 h-1/2 mx-auto mt-4">
-	<div class="bg-hero bg-cover bg-center bg-no-repeat w-full h-full" />
-	<div class="container relative bottom-20 mx-auto flex-1 justify-center text-center w-100">
-		<h1 class="text-8xl text-primary-400 border-secondary-50 text-center">Renan Braga</h1>
+<script>
+	import InfoBox from '../components/InfoBox.svelte'
+	import Hero from '../components/Hero.svelte'
+</script>
 
-		<h1 class="text-4xl font-bold tracking-tight text-secondary-400 sm:text-6xl border-tertiary-50">
-			Web Developer
-		</h1>
-	</div>
-</div>
-
-<style>
-	.bg-hero {
-		background-image: url('/170423_zha_16.png');
-		mask-image: linear-gradient(
-			to bottom,
-			rgba(0, 0, 0, 1) 75%,
-			rgba(0, 0, 0, 0.6) 90%,
-			rgba(0, 0, 0, 0)
-		);
-	}
-</style>
+<Hero />
+<InfoBox />
